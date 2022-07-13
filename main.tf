1c1bed4c5cc4d464955bcb9fe36c39b2c31e4007
Alex Bruns <alex.h.bruns@gmail.com>
2022-07-12 21:18:52 -0500

    refactor env vars
    Signed-off-by: Alex Bruns <alex.h.bruns@gmail.com>

--- main.tf
+++ main.tf
@@ -18,7 +18,9 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = var.AWS_REGION
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 # aws reference architecture https://docs.aws.amazon.com/quickstart/latest/vpc/architecture.html
