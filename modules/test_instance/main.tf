6fab6b8fa36f57646ebac795f17c3204eec94ce4
Alex Bruns <alex.h.bruns@gmail.com>
2022-07-12 20:40:47 -0500

    make public
    Signed-off-by: Alex Bruns <alex.h.bruns@gmail.com>

--- modules/test_instance/main.tf
+++ modules/test_instance/main.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "test" {
   ami = "ami-0cff7528ff583bf9a"
   instance_type = "t2.micro"
   subnet_id = var.subnet_id
+  associate_public_ip_address = true
 
   tags = {
     Name = "Test"
